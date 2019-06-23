c4b69a50c5ff9f01fe77664c1cbdc160dc139961
YJC <yangjiecong@live.com>
2019-06-23 17:06:19 +0800

    fix install-git.sh

--- setup/install-git.sh
+++ setup/install-git.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 install_for_centos() {
-    yum install -y wget curl openssl-devel curl-devel expat-devel perl-ExtUtils-CBuilder perl-ExtUtils-MakeMaker
+    yum install -y make gcc wget curl perl tk \
+        gettext-devel zlib-devel openssl-devel curl-devel expat-devel \
+        perl-ExtUtils-CBuilder perl-ExtUtils-MakeMaker
+
     cd /tmp
     file="$(curl -s 'https://mirrors.edge.kernel.org/pub/software/scm/git/' \
         | awk -F '"' '$2~/^git-[0-9].*.tar.xz$/{x=$2}END{print x}')"
@@ -9,6 +12,7 @@ install_for_centos() {
     wget "https://mirrors.edge.kernel.org/pub/software/scm/git/$file"
     )
     tar xJf "$file"
+    
     cd "${file:0:-7}"
     make prefix=/usr/local/lib/git all
     make prefix=/usr/local/lib/git install
