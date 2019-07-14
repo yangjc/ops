d83276647066792f9e609babaeca711441b6bdfa
YJC <yangjiecong@live.com>
2019-07-14 15:53:33 +0800

    tool/proxy

--- tool/certbot.sh
+++ tool/certbot.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
-( export _webroot=  _domain=  _email=  \
-&& if [ -z "$_webroot" ] || [ -z "$_domain" ] || [ -z "$_email" ]; \
-then echo "Variables undefined."; exit 1; fi \
-&& set -x \
-&& docker run -it --rm \
--v "/etc/letsencrypt:/etc/letsencrypt" \
--v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
--v "$_webroot:/tmp" \
-certbot/certbot \
-    certonly --webroot \
-    --webroot-path "/tmp" \
-    -d "$_domain" \
-    --email "$_email" \
-    --preferred-challenges "http" \
-)
+# ( export _webroot=  _domain=  _email=  \
+# && if [ -z "$_webroot" ] || [ -z "$_domain" ] || [ -z "$_email" ]; \
+# then echo "Variables undefined."; exit 1; fi \
+# && set -x \
+# && docker run -it --rm \
+# -v "/etc/letsencrypt:/etc/letsencrypt" \
+# -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+# -v "$_webroot:/tmp" \
+# certbot/certbot \
+#     certonly --webroot \
+#     --webroot-path "/tmp" \
+#     -d "$_domain" \
+#     --email "$_email" \
+#     --preferred-challenges "http" \
+# )
+
+( _run() {
+    _webroot="$1"
+    _domain="$2"
+    _email="$3"
+    if [ -z "$_webroot" ] || [ -z "$_domain" ] || [ -z "$_email" ]; then
+        echo "Unexpected arguments." >&2
+        exit 1
+    fi
+    set -x
+
+    docker run -it --rm \
+        -v "/etc/letsencrypt:/etc/letsencrypt" \
+        -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+        -v "$_webroot:/tmp" \
+        certbot/certbot \
+            certonly --webroot \
+            --webroot-path "/tmp" \
+            -d "$_domain" \
+            --email "$_email" \
+            --preferred-challenges "http"
+}
+# _run  webroot  domain  email
+_run "" "" "" )
---
+++ tool/proxy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+( _run() {
+    _url="$1"
+
+    cat <<EOF > /usr/local/bin/proxy-on
+#!/bin/bash
+if [ "\$0" == "-bash" ]; then
+    export http_proxy="$_url"
+    export https_proxy="$_url"
+    echo "HTTP(S) Proxy enabled."
+    echo "http_proxy=\$http_proxy"
+    echo "https_proxy=\$https_proxy"
+else
+    echo -e "\033[0;31mIncorrect usage. Should run:\033[0m" >&2
+    echo ". proxy-on" >&2
+fi
+EOF
+    chmod +x /usr/local/bin/proxy-on
+
+    cat <<EOF > /usr/local/bin/proxy-off
+#!/bin/bash
+if [ "\$0" == "-bash" ]; then
+    export http_proxy=
+    export https_proxy=
+    echo "HTTP(S) Proxy disabled."
+    echo "http_proxy=\$http_proxy"
+    echo "https_proxy=\$https_proxy"
+else
+    echo -e "\033[0;31mIncorrect usage. Should run:\033[0m" >&2
+    echo ". proxy-off" >&2
+fi
+EOF
+    chmod +x /usr/local/bin/proxy-off
+}
+# _run proxy-url
+_run "" )
